5aee9d3f8d49cae65be11d962b8687c8cc9620f5
Juan <69590591+TtvNekix@users.noreply.github.com>
2021-01-26 13:42:29 +0100

    Update server.lua

--- nek_id/server.lua
+++ nek_id/server.lua
@@ -1,10 +1,4 @@
 RegisterCommand('id', function(source)
 	local _source = source
 	TriggerClientEvent('esx:showNotification', _source, "Tu ID es " .._source)
-	--print(('%s: %s'):format(name, args))
 end, false)
-
-Citizen.CreateThread(function()
-	Citizen.Wait(6000)
-	print("SINPA_ID INICIADO")
-end)
